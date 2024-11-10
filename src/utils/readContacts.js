70d7156b7a6776e84f5d3bc0aea93cf0bc445190
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-11-10 22:38:23 +0100

    add read and writeContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+         const getData = await fs.readFile(PATH_DB, 'utf-8');
+         return JSON.parse(getData);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+/* readContacts();
+ */
