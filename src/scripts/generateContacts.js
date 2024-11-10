c557fdb8c74f365217dc736b8ea4aa3e316b9244
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-11-10 23:13:30 +0100

    add generate Contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,25 @@
-const generateContacts = async (number) => {};
+import * as fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import createFakeContact from "../utils/createFakeContact.js"
+
+const generateContacts = async (number) => { 
+    try {
+      // Читання наявних контактів з файлу
+      const data = await fs.readFile(PATH_DB);
+      const contacts = data ? JSON.parse(data) : []; // Якщо файл порожній, ініціалізуємо порожнім масивом
+
+      // Генерація нових контактів
+      for (let i = 0; i < number; i++) {
+        const newContact = createFakeContact();
+        contacts.push(newContact);
+      }
+      // Запис оновлених контактів у файл
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+
+    } catch (error) {
+    console.error(error);
+}
+};
+
 
 generateContacts(5);
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,4 +1,4 @@
-import * fs from 'node:fs/promises';
+
 
 export const getAllContacts = async () => { };
 
