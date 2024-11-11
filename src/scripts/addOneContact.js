d67f144278290851ba14198b75161fd37fa58bb4
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-11-11 21:30:59 +0100

    add addOneContacts

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,6 +1,20 @@
-import * fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts';
+import * as fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import createFakeContact from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => { };
+
+export const addOneContact = async () => {
+try {
+  const data = await fs.readFile(PATH_DB);
+  const contacts = data ? JSON.parse(data) : [];
+
+  //Додавання нового контакту
+  contacts.push(createFakeContact());
+
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+} catch (error) {
+    console.log(error);
+}
+ };
 
 addOneContact();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts';
 
-
-export const getAllContacts = async () => { };
+export const getAllContacts = async () => {
+    try {
+         const getContacts = await fs.readFile(PATH_DB, 'utf-8');
+        return JSON.parse(getContacts);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 console.log(await getAllContacts());
