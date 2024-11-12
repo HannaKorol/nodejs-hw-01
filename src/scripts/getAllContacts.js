581415e8007f308c3f62e0ee7adba44f142a371d
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-11-12 20:34:26 +0100

    add getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,13 +1,9 @@
 import * as fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts';
+import { PATH_DB } from '../constants/contacts.js';
 
 export const getAllContacts = async () => {
-    try {
          const getContacts = await fs.readFile(PATH_DB, 'utf-8');
         return JSON.parse(getContacts);
-    } catch (error) {
-        console.log(error);
-    }
 };
 
 console.log(await getAllContacts());
